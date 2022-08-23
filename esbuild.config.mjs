7a94465fb843d123679000df26b3530899ee657c
Damian Korcz <damikiller@gmail.com>
2022-08-23 13:38:52 +0100

    Reverted back to previous build setup

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -4,45 +4,50 @@
  * (https://github.com/jdanielmourao/obsidian-sanctum)
  */
 
-import esbuild from "esbuild";
-import process from "process";
-import { config } from "dotenv";
-import { sassPlugin } from "esbuild-sass-plugin";
-import time from 'esbuild-plugin-time';
-import { readFileSync } from "fs";
-
-config();
-
-/** Path for final file */
-const fileDev = `./theme.css`;
-
-/** Reading in text files to be added into the banner/footer */
- const license = readFileSync("./src/scss/Info/license.scss", "utf8");
- const styleSettings = readFileSync("./src/scss/Info/styleSettingsConfig.scss", "utf8");
- const hub = readFileSync("./src/scss/Info/pluginCompatibility.scss", "utf8");
-
-esbuild.build({
-    /** Entry point should be where everything is imported into. */
-    entryPoints: ["src/scss/index.scss"],
-
-    /** Banner places the content at the beginning of the bundled file. */
-    banner: {
-        css: license
-    },
-
-    /** Footer places the content at the end of the bundled file. */
-    footer: {
-        css: styleSettings + hub
-    },
-
-    /** npm run dev will watch for file changes and rebuild instantly. */
-    watch: true,
-    logLevel: "info",
-    minify: false,
-    outfile: fileDev,
-    plugins: [
-        sassPlugin(),
-        time()
-    ]
-  })
-  .catch(() => process.exit(1));
+ import esbuild from "esbuild";
+ import process from "process";
+ import { config } from "dotenv";
+ import { sassPlugin } from "esbuild-sass-plugin";
+ import time from 'esbuild-plugin-time';
+ import { readFileSync } from "fs";
+ 
+ config();
+ 
+ const prod = process.argv[2] === "production";
+ 
+ const dir = prod ? "./" : process.env.OUTDIR;
+ 
+ /** Paths for final file */
+ const fileProd = `${dir}/theme.css`;
+ const fileDev = `${dir}/PrismDev.css`;
+ 
+ /** Reading in text files to be added into the banner/footer */
+  const license = readFileSync("./src/scss/Info/license.scss", "utf8");
+  const styleSettings = readFileSync("./src/scss/Info/styleSettingsConfig.scss", "utf8");
+  const hub = readFileSync("./src/scss/Info/pluginCompatibility.scss", "utf8");
+ 
+ esbuild.build({
+     /** Entry point should be where everything is imported into. */
+     entryPoints: ["src/scss/index.scss"],
+ 
+     /** Banner places the content at the beginning of the bundled file. */
+     banner: {
+         css: license
+     },
+ 
+     /** Footer places the content at the end of the bundled file. */
+     footer: {
+         css: styleSettings + hub
+     },
+ 
+     /** npm run dev will watch for file changes and rebuild instantly. */
+     watch: !prod,
+     logLevel: "info",
+     minify: false,
+     outfile: prod ? fileProd : fileDev,
+     plugins: [
+         sassPlugin(),
+         time()
+     ]
+   })
+   .catch(() => process.exit(1));
