bf1aa92a8eaef07d438fd077896bbef637623801
Damian Korcz <damikiller@gmail.com>
2022-09-01 21:39:49 +0100

    Improved and updated the build process

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,53 +1,33 @@
-/**
- * On recommendation from @valentine195.
- * Config based on @jdanielmourao's Sanctum theme esbuild setup.
- * (https://github.com/jdanielmourao/obsidian-sanctum)
- */
+/*
+    On recommendation from @valentine195.
+    Config based on @jdanielmourao's Sanctum theme esbuild setup.
+    (https://github.com/jdanielmourao/obsidian-sanctum)
+*/
 
- import esbuild from "esbuild";
- import process from "process";
- import { config } from "dotenv";
- import { sassPlugin } from "esbuild-sass-plugin";
- import time from 'esbuild-plugin-time';
- import { readFileSync } from "fs";
- 
- config();
- 
- const prod = process.argv[2] === "production";
- 
- const dir = prod ? "./" : process.env.OUTDIR;
- 
- /** Paths for final file */
- const fileProd = `${dir}/theme.css`;
- const fileDev = `${dir}/PrismDev.css`;
- 
- /** Reading in text files to be added into the banner/footer */
-  const license = readFileSync("./src/scss/Info/license.scss", "utf8");
-  const styleSettings = readFileSync("./src/scss/Info/styleSettingsConfig.scss", "utf8");
-  const hub = readFileSync("./src/scss/Info/pluginCompatibility.scss", "utf8");
- 
- esbuild.build({
-     /** Entry point should be where everything is imported into. */
-     entryPoints: ["src/scss/index.scss"],
- 
-     /** Banner places the content at the beginning of the bundled file. */
-     banner: {
-         css: license
-     },
- 
-     /** Footer places the content at the end of the bundled file. */
-     footer: {
-         css: styleSettings + hub
-     },
- 
-     /** npm run dev will watch for file changes and rebuild instantly. */
-     watch: !prod,
-     logLevel: "info",
-     minify: false,
-     outfile: prod ? fileProd : fileDev,
-     plugins: [
-         sassPlugin(),
-         time()
-     ]
-   })
-   .catch(() => process.exit(1));
+import esbuild from "esbuild";
+import { config } from "dotenv";
+import { sassPlugin } from "esbuild-sass-plugin";
+import time from 'esbuild-plugin-time';
+
+config();
+
+const prod = process.argv[2] === "production";
+const dir = prod ? "./" : process.env.OUTDIR;
+
+/** Paths for final file */
+const fileProd = `${dir}/theme.css`;
+const fileDev = `${dir}/PrismDev.css`;
+
+esbuild.build({
+    /** Entry point should be where everything is imported into. */
+    entryPoints: ["src/scss/index.scss"],
+
+    /** npm run dev will watch for file changes and rebuild instantly. */
+    watch: !prod,
+    logLevel: "info",
+    outfile: prod ? fileProd : fileDev,
+    plugins: [
+        sassPlugin(),
+        time()
+    ]
+}).catch(() => process.exit(1));
